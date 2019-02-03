d518b6d8062e75f13aae80d1b327b79652f8ec49
mrodrigues95 <38141653+mrodrigues95@users.noreply.github.com>
2019-02-03 10:13:53 -0500

    Final commit
    Unfortunately, I completely forgot to do commits AFTER each user story, so I only have the initial and final commit available. Will take note of this for next assignment.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,44 @@
-﻿// Write your JavaScript code.
+﻿// User story 1
+// Changes the background of text to green
+function btn1_click() {
+    var input = document.getElementById("classifiedText").value;
+
+    document.getElementById('displayTextAsGreen').innerHTML = input;
+    document.getElementById('displayTextAsGreen').style.backgroundColor = "green";
+}
+
+// User story 5
+// Reverses any text the user submits
+function btn2_click() {
+    var input = document.getElementById("reversedText").value;
+
+    document.getElementById('displayTextAsReversed').innerHTML = reverseString(input);
+}
+
+function reverseString(str) {
+    var newString = "";
+
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+
+    return newString;
+}
+
+// User story 3
+// Changes any text the user submits to bold
+function btn3_click() {
+    var input = document.getElementById("boldText").value;
+    var result = input.bold();
+
+    document.getElementById('displayTextAsBold').innerHTML = result;
+}
+
+// User story 4
+// Changes any text the user submits to italics 
+function btn4_click() {
+    var input = document.getElementById("italicsText").value;
+    var result = input.italics();
+
+    document.getElementById('displayTextAsItalics').innerHTML = result;
+}
